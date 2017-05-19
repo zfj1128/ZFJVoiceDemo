a754fcf8c3c97dc404ed88cda900fc7f2fea0991
张福杰 <951151039@qq.com>
2017-05-19 15:21:55 +0800

    ZFJVoiceBubble

--- ZFJVoiceDemo/ZFJVoiceDemo/AppDelegate.swift
+++ ZFJVoiceDemo/ZFJVoiceDemo/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
+        let mvc = ViewController()
+        let nav = UINavigationController(rootViewController: mvc)
+        self.window?.rootViewController = nav
         return true
     }
 
--- ZFJVoiceDemo/ZFJVoiceDemo/ViewController.swift
+++ ZFJVoiceDemo/ZFJVoiceDemo/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,ZFJVoiceBubbleDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        uiConfig()
+    }
+    
+    lazy var zyTableView: UITableView = {
+        let tempTableView = UITableView (frame: self.view.bounds, style: UITableViewStyle.plain)
+        tempTableView.delegate = self as? UITableViewDelegate
+        tempTableView.dataSource = self as? UITableViewDataSource
+        return tempTableView
+    }()
+    
+    func uiConfig(){
+        self.title = "首页"
+        
+        let myFrame = CGRect(x: CGFloat((ScreenWidth - 250) / 2), y: CGFloat(200), width: CGFloat(150), height: CGFloat(30))
+        let voiceMegBtn = ZFJVoiceBubble.init(frame: myFrame)
+        voiceMegBtn.contentURL = URL(string: "http://7xszyu.com1.z0.glb.clouddn.com/media_blog_9250_1488873184.mp3")
+        voiceMegBtn.delegate = self
+        voiceMegBtn.isHaveBar = true
+        voiceMegBtn.userName = "墨小北"
+        view.addSubview(voiceMegBtn)
+    }
+    
+    // MARK: ZFJVoiceBubbleDelegate
+    func voiceBubbleStratOrStop(_ isStart: Bool) {
+        NSLog("voiceBubbleStratOrStop")
+    }
+    
+    func voiceBubbleDidStartPlaying(_ voiceBubble: ZFJVoiceBubble) {
+        NSLog("voiceBubbleDidStartPlaying")
     }
 
     override func didReceiveMemoryWarning() {
